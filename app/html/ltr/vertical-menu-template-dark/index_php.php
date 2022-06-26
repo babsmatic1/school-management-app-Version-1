dc3db6c8207312ced849bab976ab5312df96afc8
carleii <devcarle@gmail.com>
2022-06-26 11:06:46 +0200

    error and warning

--- app/html/ltr/vertical-menu-template-dark/index_php.php
+++ app/html/ltr/vertical-menu-template-dark/index_php.php
@@ -1,8 +1,8 @@
 <?php
 //DISABLE WARNING
-// error_reporting(E_ERROR | E_PARSE);
+error_reporting(E_ERROR | E_PARSE);
 //DISABLE ERROR
-// error_reporting(0);
+error_reporting(0);
 require 'classe_package.php';
 require 'database_connection.php';
 // VERIFY IF THE COOKIE EXIST EITHER GO TO THE LOGIN PAGE
